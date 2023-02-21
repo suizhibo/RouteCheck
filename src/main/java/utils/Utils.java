8cab32b5e3bd657c6bb24a06ef6428d7b90cb0f0
szb <1483221711@qq.com>
2023-02-21 17:49:44 +0800

    [Fix] 修复中文乱码

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -3,6 +3,7 @@ package utils;
 import java.io.*;
 import java.math.BigInteger;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
@@ -29,8 +30,8 @@ public class Utils {
     }
 
     public static void fileWriter(String filepath, String content) throws IOException {
-        try (FileWriter fileWriter = new FileWriter(filepath)) {
-            fileWriter.append(content);
+        try (PrintWriter writer = new PrintWriter(filepath, "UTF-8")) {
+            writer.println(content);
         }
     }
 
